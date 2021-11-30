7eb627276532b17d9bb9a2ac199938b75014053c
Anamika Agrawal <78848657+anamikaagrawal123@users.noreply.github.com>
2021-11-30 16:47:36 +0530

    Update code_setup.sh

--- scripts/code_setup.sh
+++ scripts/code_setup.sh
@@ -14,7 +14,6 @@
     . "${ONE_PIPELINE_PATH}/tools/get_repo_params"
     APP_TOKEN_PATH="./app-token"
     read -r APP_REPO_NAME APP_REPO_OWNER APP_SCM_TYPE APP_API_URL < <(get_repo_params "$(get_env APP_REPO)" "$APP_TOKEN_PATH")
-    REPO=${APP_REPO_NAME%.git}
     APP_TOKEN=$(cat $APP_TOKEN_PATH)
-    curl -u ":$APP_TOKEN" https://api.github.com/repos/$OWNER/$REPO/branches/master/protection -XPUT -d '{"required_pull_request_reviews":{"dismiss_stale_reviews":true},"required_status_checks":{"strict":true,"contexts":["tekton/code-branch-protection","tekton/code-unit-tests","tekton/code-cis-check","tekton/code-vulnerability-scan","tekton/code-detect-secrets"]},"enforce_admins":null,"restrictions":null}'
+    curl -u ":$APP_TOKEN" https://api.github.com/repos/$APP_REPO_OWNER/$APP_REPO_NAME/branches/master/protection -XPUT -d '{"required_pull_request_reviews":{"dismiss_stale_reviews":true},"required_status_checks":{"strict":true,"contexts":["tekton/code-branch-protection","tekton/code-unit-tests","tekton/code-cis-check","tekton/code-vulnerability-scan","tekton/code-detect-secrets"]},"enforce_admins":null,"restrictions":null}'
     npm ci
